f277b7893bd07c305e59b14cbbb3049155c4ed03
AbdelI <homeworkoracle@protonmail.com>
2020-05-27 10:12:04 +0200

    two_sum,done

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -1,8 +1,23 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+        def twoSum(self,nums, target):
+            """
+            :type nums: List[int]
+            :type target: int
+            :rtype: List[int]
+            """
+
+            for i1 in range(len(nums)):
+                if nums[i1] > target:
+                    continue
+
+                for i2 in range(i1 + 1, len(nums)):
+                    if nums[i1] + nums[i2] == target:
+                        return [i1, i2]
+
+            return None
+
+
+stuff = Solution()
+
+print(stuff.twoSum([2,7,11,15],9))
+
